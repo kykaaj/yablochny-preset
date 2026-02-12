e09aae0dcd8fa03e57097e171225149867a54f56
KykaaJ <kykaaj@gmail.com>
2026-02-12 18:31:23 +0700

    License: Added CC BY-NC-SA 4.0 license and copyright header

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/*
+ * Yablochny Preset Extension for SillyTavern
+ * Copyright (c) 2026 Kykaaj
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ 
+ * or see the LICENSE file in this directory.
+ *
+ * Attribution: You must give appropriate credit to Kykaaj if you redistribute or adapt this work.
+ * NonCommercial: You may not use this material for commercial purposes.
+ * ShareAlike: If you remix, transform, or build upon the material, you must distribute your contributions under the same license.
+ */
+
 import { extension_settings } from "/scripts/extensions.js";
 import { saveSettingsDebounced } from "/script.js";
 import { getCurrentLocale } from "/scripts/i18n.js";
